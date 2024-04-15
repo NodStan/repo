f5906be0a46c00c2207da6e3c5ebe65b6e5f8dd3
Stanley <stanleyobitunwase@gmail.com>
2024-04-15 13:21:08 +0100

    first commit

---
+++ footBallClubArray.java
@@ -0,0 +1,27 @@
+//package assignment2;
+//
+//import java.util.ArrayList;
+//
+//public class footBallClubArray {
+//    ArrayList<String> list = new ArrayList<>();
+//
+//    public static void main(String[] args) {
+//        ArrayList<String> playerList = new ArrayList<>();
+//        playerList.add("GK");
+//        playerList.add("RB");
+//        playerList.add("CB");
+//        playerList.add("CB");
+//        playerList.add("LB");
+//        playerList.add("CDM");
+//        playerList.add("CM");
+//        playerList.add("CM");
+//        playerList.add("RW");
+//        playerList.add("ST");
+//        playerList.add("LW");
+//
+//        footBallClub Chelsea = new Chelsea();
+//        System.out.println();
+//
+//        }
+//    }
+//}
